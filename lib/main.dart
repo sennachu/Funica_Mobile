94d03904cb7912082ebbb99adde3f0e38652198c
Sena Bezirkan <90784360+sennachu@users.noreply.github.com>
2024-04-09 21:21:19 +0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello funica!'),
+          child: Text('Hi funica!'),
         ),
       ),
     );
