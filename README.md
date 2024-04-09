082255dc91c9358f05f2fdefca588260e05a9bd1
Sena Bezirkan <90784360+sennachu@users.noreply.github.com>
2024-04-09 21:12:06 +0300

    project created

---
+++ README.md
@@ -0,0 +1,3 @@
+# funica_mobile
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/funica_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.funica_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
