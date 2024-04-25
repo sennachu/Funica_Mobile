acba6df21ce77e9293392217d5281345a8b2917e
Deniz Yıldız <denizz.yldz8@gmail.com>
2024-04-26 02:55:44 +0300

    favorite cubit added

--- lib/model/product_card.dart
+++ lib/model/product_card.dart
@@ -1,135 +1,164 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart'; // Google Fonts paketini ekledik
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:funica_mobile/bloc/favorite/products_cubit.dart';
+import 'package:funica_mobile/model/product.dart';
+import 'package:funica_mobile/model/product.dart';
+import 'package:funica_mobile/model/product.dart';
+import 'package:funica_mobile/model/product.dart';
+import 'package:funica_mobile/model/product.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:funica_mobile/model/product.dart';
 import 'package:funica_mobile/model/product_detail.dart';
-import 'package:gap/gap.dart';
-import 'package:grock/grock.dart';
 
-class ProductCard extends StatelessWidget {
-  Product product;
+import '../bloc/cart/cart_cubit.dart';
+import 'product.dart';
+
+class ProductCard extends StatefulWidget {
+  final Product product;
 
   ProductCard({required this.product});
 
   @override
+  State<ProductCard> createState() => _ProductCardState();
+}
+
+class _ProductCardState extends State<ProductCard> {
+  late CartCubit cartCubit;
+  late ProductsCubit productsCubit;
+
+  @override
+  void initState() {
+    super.initState();
+    cartCubit = context.read<CartCubit>();
+    productsCubit = context.read<ProductsCubit>();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: GestureDetector(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => ProductDetail(product: product),
-            ),
-          );
-        },
-        child: Container(
-          decoration: BoxDecoration(
-            color: Theme.of(context).brightness == Brightness.dark
-                ? Colors.black
-                : Colors.white,
-            borderRadius: BorderRadius.circular(15),
-            boxShadow: [
-              BoxShadow(
-                color: const Color.fromARGB(66, 35, 35, 35),
-                blurRadius: 7,
-              ),
-            ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ProductDetail(product: widget.product),
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ClipRRect(
-                borderRadius:
-                    BorderRadius.circular(10.0), // Kenar yarıçapını belirleyin
-                child: Hero(
-                  tag: product.photo,
-                  child: Image.asset(
-                    product.photo,
-                    height: 150,
-                    width: 250,
-                    fit: BoxFit.cover,
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Color.fromARGB(211, 255, 255, 255),
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: const Color.fromARGB(66, 35, 35, 35),
+              blurRadius: 7,
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
+                  child: Hero(
+                    tag: widget.product.photo,
+                    child: Image.asset(
+                      widget.product.photo,
+                      height: 150,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: 7.horizontalP,
-                child: Row(
-                  children: [
-                    Text(
-                      "${product.name}",
-                      style: GoogleFonts.poppins(
-                        // Poppins yazı tipini burada kullanıyoruz
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
-                      ),
+                if (productsCubit
+                            .isFavorite(products[index]["id"] as int))
+                          IconButton(
+                              onPressed: () {
+                                productsCubit.removeFromFavorites(
+                                    products[index]["id"] as int);
+                              },
+                              icon: const Icon(
+                                Icons.favorite,
+                                color: Colors.red,
+                              ))
+                        else
+                          IconButton(
+                              onPressed: () {
+                                productsCubit.addToFavorites(products[index]);
+                              },
+                              icon: const Icon(Icons.favorite_border))
+                      ],
                     ),
-                  ],
-                ),
-              ),
-              Gap(2),
-              Padding(
-                padding: 7.horizontalP,
-                child: Row(
-                  children: [
-                    Icon(
-                      product.icon,
-                      size: 20,
+                  
+                
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "${widget.product.name}",
+                    style: GoogleFonts.poppins(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
                     ),
-                    SizedBox(width: 6),
-                    Text(
-                      product.puan,
-                      style: GoogleFonts.poppins(
-                        color: Theme.of(context).brightness == Brightness.dark
-                            ? Colors.white
-                            : Colors.black,
+                  ),
+                  SizedBox(height: 2),
+                  Row(
+                    children: [
+                      Icon(
+                        widget.product.icon,
+                        size: 20,
                       ),
-                    ),
-                    SizedBox(width: 5),
-                    Text(
-                      product.tire,
-                      style: GoogleFonts.poppins(
-                        color: Theme.of(context).brightness == Brightness.dark
-                            ? Colors.white
-                            : Colors.black,
-                      ),
-                    ),
-                    SizedBox(width: 10),
-                    Container(
-                      width: 55,
-                      height: 20,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        color: Theme.of(context).brightness == Brightness.dark
-                            ? Color.fromARGB(107, 117, 117, 117)
-                            : Colors.white,
-                      ),
-                      child: Center(
-                        child: Text(
-                          product.sold,
-                          style: GoogleFonts.poppins(
-                              fontSize: 8, fontWeight: FontWeight.bold),
+                      SizedBox(width: 6),
+                      Text(
+                        widget.product.puan,
+                        style: GoogleFonts.poppins(
+                          color: Colors.black54,
+                          fontSize: 12,
                         ),
                       ),
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(height: 3),
-              Row(
-                children: [
-                  Padding(
-                    padding: 10.horizontalP,
-                    child: Text(
-                      product.price,
-                      style: GoogleFonts.poppins(
-                          fontSize: 12, fontWeight: FontWeight.bold),
+                      SizedBox(width: 5),
+                      Text(
+                        widget.product.tire,
+                        style: GoogleFonts.poppins(color: Colors.black87),
+                      ),
+                      SizedBox(width: 10),
+                      Container(
+                        width: 55,
+                        height: 20,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Color.fromARGB(149, 232, 232, 232),
+                        ),
+                        child: Center(
+                          child: Text(
+                            widget.product.sold,
+                            style: GoogleFonts.poppins(
+                              fontSize: 8,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 3),
+                  Text(
+                    widget.product.price,
+                    style: GoogleFonts.poppins(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
