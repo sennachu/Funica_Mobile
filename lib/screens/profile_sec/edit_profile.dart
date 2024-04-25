5240ad79d5e26d7f07ef4c89e951a6d5a56c313c
Sena Bezirkan <90784360+sennachu@users.noreply.github.com>
2024-04-25 14:38:56 +0300

    edit profile screen designed

--- lib/screens/profile_sec/edit_profile.dart
+++ lib/screens/profile_sec/edit_profile.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:funica_mobile/widgets/bottomNavigation.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class EditProfileScreen extends StatefulWidget {
-  const EditProfileScreen({super.key});
+  const EditProfileScreen({Key? key}) : super(key: key);
 
   @override
   State<EditProfileScreen> createState() => _EditProfileScreenState();
 }
 
 class _EditProfileScreenState extends State<EditProfileScreen> {
+  String _firstName = '';
+  String _lastName = '';
+  DateTime? _selectedDate;
+  String _email = '';
+  String _selectedCountry = 'United States';
+  String _phoneNumber = '';
+  String _selectedGender = 'Male';
+
+  void _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1900),
+      lastDate: DateTime.now(),
+    );
+    if (picked != null && picked != _selectedDate) {
+      setState(() {
+        _selectedDate = picked;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,28 +45,201 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
         ),
         title: Text("Edit Profile Screen"),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Color.fromARGB(9, 137, 137, 137),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              InkWell(
+                onTap: () {
+                  // Handle tap
+                },
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'First Name',
+                    labelStyle: GoogleFonts.poppins(),
+                    border: InputBorder.none,
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      _firstName = value;
+                    });
+                  },
                 ),
               ),
-              child: Center(child: Text("Edit profile Screen")),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Color.fromARGB(9, 137, 137, 137),
+              SizedBox(height: 5.0),
+              InkWell(
+                onTap: () {
+                  // Handle tap
+                },
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Last Name',
+                    labelStyle: GoogleFonts.poppins(),
+                    border: InputBorder.none,
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      _lastName = value;
+                    });
+                  },
+                ),
               ),
-            ),
+              SizedBox(height: 5.0),
+              GestureDetector(
+                onTap: () => _selectDate(context),
+                child: AbsorbPointer(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Date of Birth',
+                      labelStyle: GoogleFonts.poppins(),
+                      suffixIcon: Icon(Icons.calendar_today),
+                      border: InputBorder.none,
+                    ),
+                    controller: TextEditingController(
+                      text: _selectedDate != null
+                          ? '${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}'
+                          : '',
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 5.0),
+              Row(
+                children: [
+                  Expanded(
+                    child: InkWell(
+                      onTap: () {
+                        // Handle tap
+                      },
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          labelStyle: GoogleFonts.poppins(),
+                          suffixIcon:
+                              Icon(Icons.mail_outline, color: Colors.black),
+                          contentPadding: EdgeInsets.symmetric(vertical: 16.0),
+                          border: InputBorder.none,
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            _email = value;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 5.0),
+              DropdownButtonFormField<String>(
+                value: _selectedCountry,
+                onChanged: (value) {
+                  setState(() {
+                    _selectedCountry = value!;
+                  });
+                },
+                items: ['United States', 'Turkey']
+                    .map<DropdownMenuItem<String>>(
+                        (String value) => DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            ))
+                    .toList(),
+                decoration: InputDecoration(
+                  labelText: 'Country',
+                  labelStyle: GoogleFonts.poppins(),
+                ),
+              ),
+              SizedBox(height: 5.0),
+              Row(
+                children: [
+                  DropdownButton<String>(
+                    value: '+1',
+                    onChanged: (value) {
+                      setState(() {
+                        // Country code selection logic
+                      });
+                    },
+                    items: ['+1', '+90']
+                        .map<DropdownMenuItem<String>>(
+                            (String value) => DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(value),
+                                ))
+                        .toList(),
+                  ),
+                  SizedBox(width: 5.0),
+                  Expanded(
+                    child: InkWell(
+                      onTap: () {
+                        // Handle tap
+                      },
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Phone Number',
+                          labelStyle: GoogleFonts.poppins(),
+                          border: InputBorder.none,
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            _phoneNumber = value;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 5.0),
+              DropdownButtonFormField<String>(
+                value: _selectedGender,
+                onChanged: (value) {
+                  setState(() {
+                    _selectedGender = value!;
+                  });
+                },
+                items: ['Male', 'Female']
+                    .map<DropdownMenuItem<String>>(
+                        (String value) => DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            ))
+                    .toList(),
+                decoration: InputDecoration(
+                  labelText: 'Gender',
+                  labelStyle: GoogleFonts.poppins(),
+                ),
+              ),
+              SizedBox(height: 37.0),
+              Center(
+                child: InkWell(
+                  onTap: () {
+                    // Handle tap
+                  },
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Handle form submission
+                      },
+                      child: Text('Update'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        foregroundColor: Colors.white,
+                        textStyle: GoogleFonts.poppins(color: Colors.black),
+                        // Customize the button color here
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-          BottomNavigator(selectedIndex: 4),
-        ],
+        ),
       ),
+      bottomNavigationBar: BottomNavigator(selectedIndex: 4),
     );
   }
 }
