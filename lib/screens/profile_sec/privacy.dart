4afb9d2876592377da4dbab4e52ff6af406ad0d6
Sena Bezirkan <90784360+sennachu@users.noreply.github.com>
2024-04-25 14:47:39 +0300

    privacy screen designed

--- lib/screens/profile_sec/privacy.dart
+++ lib/screens/profile_sec/privacy.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:funica_mobile/widgets/bottomNavigation.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PrivacyScreen extends StatefulWidget {
-  const PrivacyScreen({super.key});
+  const PrivacyScreen({Key? key}) : super(key: key);
 
   @override
   State<PrivacyScreen> createState() => _PrivacyScreenState();
@@ -24,20 +25,66 @@ class _PrivacyScreenState extends State<PrivacyScreen> {
       ),
       body: Column(
         children: [
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Color.fromARGB(9, 137, 137, 137),
-                ),
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              "1. Types of Data Collect",
+              style: GoogleFonts.poppins(
+                fontWeight: FontWeight.bold, // Bold text
+                fontSize: 18,
               ),
-              child: Center(child: Text("Privacy")),
+            ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                // Lorem ipsum text
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultrices lobortis justo, in venenatis massa condimentum id. Aliquam in ultricies metus. Proin tincidunt, arcu non sodales pharetra, dolor velit feugiat libero, eu fringilla neque orci a sem. Nullam nec blandit dolor. Nam ultricies posuere ipsum nec interdum. Sed vehicula nunc id tellus tincidunt, eget ultricies nunc auctor. Aliquam erat volutpat. Sed nec est a lectus condimentum posuere a nec enim. Sed rhoncus augue at justo scelerisque, vel fermentum metus finibus. Duis lobortis nisi ut dolor vestibulum, id consequat libero imperdiet.",
+                style: GoogleFonts.poppins(fontSize: 16),
+              ),
             ),
           ),
           Container(
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Color.fromARGB(9, 137, 137, 137),
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              "2. Use of Your Personel Data",
+              style: GoogleFonts.poppins(
+                fontWeight: FontWeight.bold, // Bold text
+                fontSize: 18,
+              ),
+            ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                // Lorem ipsum text
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultrices lobortis justo, in venenatis massa condimentum id. Aliquam in ultricies metus. Proin tincidunt, arcu non sodales pharetra, dolor velit feugiat libero, eu fringilla neque orci a sem. Nullam nec blandit dolor. Nam ultricies posuere ipsum nec interdum. Sed vehicula nunc id tellus tincidunt, eget ultricies nunc auctor. Aliquam erat volutpat. Sed nec est a lectus condimentum posuere a nec enim. Sed rhoncus augue at justo scelerisque, vel fermentum metus finibus. Duis lobortis nisi ut dolor vestibulum, id consequat libero imperdiet.",
+                style: GoogleFonts.poppins(fontSize: 16),
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              "3. Disclosure of Your Personel Data",
+              style: GoogleFonts.poppins(
+                fontWeight: FontWeight.bold, // Bold text
+                fontSize: 18,
+              ),
+            ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                // Lorem ipsum text
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultrices lobortis justo, in venenatis massa condimentum id. Aliquam in ultricies metus. Proin tincidunt, arcu non sodales pharetra, dolor velit feugiat libero, eu fringilla neque orci a sem. Nullam nec blandit dolor. Nam ultricies posuere ipsum nec interdum. Sed vehicula nunc id tellus tincidunt, eget ultricies nunc auctor. Aliquam erat volutpat. Sed nec est a lectus condimentum posuere a nec enim. Sed rhoncus augue at justo scelerisque, vel fermentum metus finibus. Duis lobortis nisi ut dolor vestibulum, id consequat libero imperdiet.",
+                style: GoogleFonts.poppins(fontSize: 16),
               ),
             ),
           ),
